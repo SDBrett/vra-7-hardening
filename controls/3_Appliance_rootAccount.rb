8603f7c4ac5a617bb24ce654b899c8900cd52515
SDBrett <brett008@gmail.com>
2017-12-22 10:22:20 +1100

    Control name updates

--- controls/3_Appliance_rootAccount.rb
+++ controls/3_Appliance_rootAccount.rb
@@ -1,4 +1,4 @@
-control 'vra7-appliance-rootAccount-3.1' do
+control '3_appliance-rootAccount-3.1' do
   title 'Ensure password history and retry limits are set'
   desc "Validates failed login retry limit and password history"
   describe file('/etc/pam.d/common-password') do
@@ -7,7 +7,7 @@ control 'vra7-appliance-rootAccount-3.1' do
   end
 end
 
-control 'vra7-appliance-rootAccount-3.2' do
+control '3_appliance-rootAccount-3.2' do
   title 'Ensure use_authok is configured'
   desc  'When password changing enforce the module to set the new password to the one provided by a previously stacked password module'
   describe file("/etc/pam.d/common-password") do
@@ -15,7 +15,7 @@ control 'vra7-appliance-rootAccount-3.2' do
   end
 end
 
-control 'vra7-appliance-rootAccount-3.3' do
+control '3_appliance-rootAccount-3.3' do
   title 'Ensure root password hashing algorithm is SHA-512'
   desc  'The use of SHA-512 encryption for the root password provides stronger protection about brute force attacks'
   describe file("/etc/shadow") do
@@ -23,7 +23,7 @@ control 'vra7-appliance-rootAccount-3.3' do
   end
 end
  
-control 'vra7-appliance-rootAccount-3.3' do
+control '3_appliance-rootAccount-3.3' do
   title 'Check that root password expiration time has not been increased above default of 365 days'
   desc  'The root account has a default password expiration of 365 days. For hardended systems this should be reduced to 90 days'
   describe shadow.users('root') do
