8d071d43963f15fa63674f6540d89abc3aac7d5e
SDBrett <brett008@gmail.com>
2018-01-08 12:20:25 +1100

    Added FIPS and additional account checks

--- controls/6_Appliance_VAMI.rb
+++ controls/6_Appliance_VAMI.rb
@@ -1,7 +1,16 @@
 control '6_Appliance_VAMI_6.1' do
-    title 'Verify at least one additional VAMI user has been created'
-    desc 'The root user account for the Virtual Appliance Management Interface uses PAM for authentication, so the clipping levels set by PAM also apply. If you have not appropriately isolated the Virtual Appliance Management Interface, a lock out of the system root account could occur if an attacker attempts to brute force the login. In addition, where the root account is considered insufficient to provide non-repudiation by more than one person in your organization, then you might elect to change the admin user for the management interface.'
-    describe etc_group.where(name: 'vami').users do
-        its('count') { should be > 2}
+    title 'Verify root account as been removed from vami group'
+    desc 'An additional VAMI user account should be created and added to the vami group. After this has been done, it is recommended that the root account is removed from the vami group.'
+    describe etc_group.where(name: 'vami') do
+            its(users) { should_not include 'root' }
     end
 end
+
+
+control '1_Appliance_VAMI_6.2' do
+    title 'Validate if FIPS has been enabled'
+    desc 'The vRealize Automation appliance now uses the Federal Information Processing Standard (FIPS) 140-2 certified version of OpenSSL for data-in-transit over TLS on all inbound and outbound network traffic.'
+    describe command ('vcac-vami fips status') do
+      its('stdout') {should_not match ('FIPS is DISABLED!')}
+    end
+  end
