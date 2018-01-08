104d6dfbc91a4640953b4b754d7423adba49d56d
SDBrett <brett008@gmail.com>
2018-01-08 12:31:32 +1100

    Add check for SSLv3

--- controls/1_Appliance.rb
+++ controls/1_Appliance.rb
@@ -3,9 +3,6 @@ control 'cis-dil-benchmark-1.4.2' do
   desc  "Setting the boot loader password will require that anyone rebooting the system must enter a password before being able to set command line boot parameters\n\nRationale: Requiring a boot password upon execution of the boot loader will prevent an unauthorized user from entering boot parameters or changing the boot partition. This prevents users from weakening security (e.g. turning off SELinux at boot time)."
   impact 1.0
 
-  tag cis: 'distribution-independent-linux:1.4.2'
-  tag level: 1
-
   describe.one do
     %w(/boot/grub/grub.conf /boot/grub/grub.cfg /boot/grub/menu.lst /boot/boot/grub/grub.conf /boot/boot/grub/grub.cfg /boot/boot/grub/menu.lst).each do |f|
       describe file(f) do
@@ -13,5 +10,19 @@ control 'cis-dil-benchmark-1.4.2' do
         its(:content) { should match(/^password/) }
       end
     end
+  end
+end
+
+
+control '1_Appliance_1.2' do
+  title 'validate SSLv3 is disabled'
+  desc 'As part of your hardening process, ensure that the deployed vRealize Automation appliance uses secure transmission channels.'
+  
+  describe file('/etc/haproxy/conf.d/20-vcac.cfg') do
+  its('content') {should include 'no-sslv3'}
+  end
+
+  describe file('/etc/haproxy/conf.d/30-vro-config.cfg') do
+  its('content') {should include 'no-sslv3'}
   end
 end
