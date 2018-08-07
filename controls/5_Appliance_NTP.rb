a4aef02e070677c376ff8e0e90c05373c8eedba5
SDBrett <brett008@gmail.com>
2018-08-08 09:40:59 +1000

    fix service checks and removed placeholder

--- controls/5_Appliance_NTP.rb
+++ controls/5_Appliance_NTP.rb
@@ -8,7 +8,6 @@ control '5_Appliance_NTP_5.1' do
     its('mode') { should cmp '0640' }
   end
   describe ntp_conf('/etc/ntp.conf') do
-    its('setting_name') { should eq 'value' }
     its('restrict') { should include 'default kod nomodify notrap nopeer noquery' }
     its('restrict') { should include '-6 default kod nomodify notrap nopeer noquery' }
     its('restrict') { should include '127.0.0.1' }
@@ -20,6 +19,9 @@ control '5_Appliance_NTP_5.2' do
   title 'Validate permissions and configuration of /etc/ntp.conf file'
   describe package 'ntp' do
     it { should be_installed }
+  end
+
+  describe service('ntp') do
     it { should be_enabled }
     it { should be_running }
   end
